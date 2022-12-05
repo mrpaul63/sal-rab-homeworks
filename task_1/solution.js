594bef80a19c451cfb50f6b175ee6c844ba36c09
Paul <mrpaul63@gmail.com>
2022-12-05 14:17:36 +0400

    cart amount

--- task_1/solution.js
+++ task_1/solution.js
@@ -43,6 +43,8 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽'
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
