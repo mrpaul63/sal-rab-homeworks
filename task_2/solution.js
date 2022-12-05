9f2860f86c8822f1d84c0c28b6175f762f403423
Paul <mrpaul63@gmail.com>
2022-12-05 20:07:55 +0400

    shippingSum5

--- task_2/solution.js
+++ task_2/solution.js
@@ -10,7 +10,9 @@ function calcShipping(sum, min, shipping) {
     if (productsSum = 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     }
-    else shippingSum = shippingPrice
+    else {
+        shippingSum = shippingPrice;
+    }
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
 
